7db35bea26bcadb818e03eacbeab34cb2468e438
Gigi-Pons <grecia.bueno@icloud.com>
2025-05-13 18:31:34 -0600

    added additional classes

---
+++ src/main/java/com/gbueno/store/StripePaymentService.java
@@ -0,0 +1,16 @@
+package com.gbueno.store;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class StripePaymentService implements PaymentService {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIPE");
+        System.out.println("Amount: " + amount);
+    }
+
+}
